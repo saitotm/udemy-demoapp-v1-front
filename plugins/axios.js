5ca9f9a5fbbd5623070ba114516e7de036eea0d9
saitotm <tomokazu.ml.tech@gmail.com>
2021-04-05 00:14:45 +0900

    add axios.js

---
+++ plugins/axios.js
@@ -0,0 +1,16 @@
+export default ({ $axios }) => {
+    // リクエストログ
+    $axios.onRequest((config) => {
+        console.log(config)
+    })
+
+    // レスポンスログ
+    $axios.onResponse((config) => {
+        console.log(config)
+    })
+
+    // エラーログ
+    $axios.onError((e) => {
+        console.log(e.response)
+    })
+}
