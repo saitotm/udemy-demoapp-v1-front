397e980a4ed519b297f8aac7a39699df5697cd34
saitotm <tomokazu.ml.tech@gmail.com>
2021-04-06 15:54:33 +0900

    vuetify setup

--- Dockerfile
+++ Dockerfile
@@ -2,14 +2,14 @@ FROM node:14.4.0-alpine
 
 ARG WORKDIR
 ARG CONTAINER_PORT
-
 ARG API_URL
 
 ENV HOME=/${WORKDIR} \
     LANG=C.UTF-8 \
     TZ=Asia/Tokyo \
     HOST=0.0.0.0 \
-    API_URL=${API_URL}
+    API_URL=${API_URL} \
+    NPM_CONFIG_PRODUCTION=false
 
 RUN echo ${HOME}
 RUN echo ${CONTAINER_PORT}
@@ -18,7 +18,11 @@ RUN echo ${API_URL}
 WORKDIR ${HOME}
 
 COPY package*.json ./
-RUN yarn install
+
+RUN apk update && \
+    apk upgrade && \
+    apk add --no-cache make gcc g++ python && \
+    yarn install
 
 COPY . ./
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,42 +1,73 @@
 <template>
-  <div>
-    <h2>
-      Usersテーブルの取得
-    </h2>
-    <table v-if="users.length">
-      <thead>
-        <tr>
-          <th>id</th>
-          <th>name</th>
-          <th>email</th>
-          <th>created_at</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr
-          v-for="(user, i) in users"
-          :key="`user-${i}`"
+  <v-container fluid>
+    <v-card
+      flat
+      tile
+      color="transparent"
+    >
+      <v-card-title>
+        Usersテーブルの取得
+      </v-card-title>
+      <v-card-text>
+        <v-simple-table dense>
+          <template
+            v-if="users.length"
+          >
+            <thead>
+              <tr>
+                <th
+                  v-for="(key, i) in userKeys"
+                  :key="`key-${i}`"
+                >
+                  {{ key }}
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr
+                v-for="(user, i) in users"
+                :key="`user-${i}`"
+              >
+                <td>{{ user.id }}</td>
+                <td>{{ user.name }}</td>
+                <td>{{ user.email }}</td>
+                <td>{{ dateFormat(user.created_at) }}</td>
+              </tr>
+            </tbody>
+          </template>
+          <template v-else>
+            ユーザーが存在しません
+          </template>
+        </v-simple-table>
+      </v-card-text>
+      <v-card-title>
+        Vuetifyの導入
+      </v-card-title>
+      <v-card-text>
+        <v-btn
+          v-for="(color, i) in colors"
+          :key="`color-${i}`"
+          :color="color"
+          class="mr-2"
         >
-          <td>{{ user.id }}</td>
-          <td>{{ user.name }}</td>
-          <td>{{ user.email }}</td>
-          <td>{{ dateFormat(user.created_at) }}</td>
-        </tr>
-      </tbody>
-    </table>
-
-    <div v-else>
-      ユーザーが取得出来ませんでした
-    </div>
-  </div>
+          {{ color }}
+        </v-btn>
+      </v-card-text>
+    </v-card>
+  </v-container>
 </template>
 
 <script>
 export default {
   async asyncData ({ $axios }) {
-    let users = []
-    users = await $axios.$get('/api/v1/users')
-    return { users }
+    const users = await $axios.$get('/api/v1/users')
+    const userKeys = Object.keys(users[0] || {})
+    return { users, userKeys }
+  },
+  data () {
+    return {
+      colors: ['primary', 'info', 'success', 'warning', 'error', 'background']
+    }
   },
   computed: {
     dateFormat () {
