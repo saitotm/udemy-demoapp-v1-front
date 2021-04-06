23ca48ed4386d003100aa5e9c5fde44ef9a69496
saitotm <tomokazu.ml.tech@gmail.com>
2021-04-06 16:26:27 +0900

    add vuetify custom css

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,6 +20,7 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    '~/assets/sass/main.scss'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
@@ -47,6 +48,8 @@ export default {
   axios: {},
 
   vuetify: {
+    treeShake: true,
+    customVariables: ['~/assets/sass/variable.scss'], // treeShakeがtrueの場合のみ有効
     theme: {
       themes: {
         light: {
@@ -59,7 +62,7 @@ export default {
         }
       }
     }
-  },
+  }, 
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
