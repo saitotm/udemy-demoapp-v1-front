433351a3b0775913e9a45f43282712e52868fbbf
saitotm <tomokazu.ml.tech@gmail.com>
2021-04-07 19:51:01 +0900

    signup page set data building

--- pages/signup.vue
+++ pages/signup.vue
@@ -4,9 +4,15 @@
       <v-form
         v-model="isValid"
       >
-        <user-form-name />
-        <user-form-email />
-        <user-form-password />
+        <user-form-name
+          :name.sync="params.user.name"
+        />
+        <user-form-email
+          :email.sync="params.user.email"
+        />
+        <user-form-password
+          :password.sync="params.user.password"
+        />
         <v-btn
           :disabled="!isValid"
           block
@@ -16,6 +22,9 @@
           登録する
         </v-btn>
       </v-form>
+      <v-card-text>
+        {{ params }}
+      </v-card-text>
     </template>
   </bef-login-form-card>
 </template>
@@ -31,7 +40,8 @@ export default {
   layout: 'beforeLogin',
   data () {
     return {
-      isValid: false
+      isValid: false,
+      params: { user: { name: '', email: '', password: '' } }
     }
   }
 }
