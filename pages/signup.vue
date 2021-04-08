79289949220cb6c2aab6b0bbdd6e1028b8eb49cf
saitotm <tomokazu.ml.tech@gmail.com>
2021-04-08 17:20:07 +0900

    finish signup page

--- pages/signup.vue
+++ pages/signup.vue
@@ -2,6 +2,7 @@
   <bef-login-form-card>
     <template #form-card-content>
       <v-form
+        ref="form"
         v-model="isValid"
       >
         <user-form-name
@@ -14,17 +15,16 @@
           :password.sync="params.user.password"
         />
         <v-btn
-          :disabled="!isValid"
+          :disabled="!isValid || loading"
+          :loading="loading"
           block
           color="myblue"
           class="white--text"
+          @click="signup"
         >
           登録する
         </v-btn>
       </v-form>
-      <v-card-text>
-        {{ params }}
-      </v-card-text>
     </template>
   </bef-login-form-card>
 </template>
@@ -41,7 +41,21 @@ export default {
   data () {
     return {
       isValid: false,
+      loading: false,
       params: { user: { name: '', email: '', password: '' } }
+    }
+  },
+  methods: {
+    signup () {
+      this.loading = true
+      setTimeout(() => {
+        this.formReset()
+        this.loading = false
+      }, 1500)
+    },
+    formReset () {
+      this.$refs.form.reset()
+      this.params = { user: { name: '', email: '', password: '' } }
     }
   }
 }
